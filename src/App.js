0787c4455bf968a2f0400ccb69356b07e23e44a9
govindgodara1310 <govindgodara14314@gmail.com>
2025-11-03 14:41:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
+import Work from "./components/Work";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import BlurBlop from "./BlurBlop";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#050414]">
+
+      <BlurBlop position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+
+      <div className="relative pt-20">
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
